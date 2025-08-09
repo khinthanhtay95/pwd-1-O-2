29a35e3f17e422911e5824d4efe642657c836e65
Ei Maung <eimg@fairwayweb.com>
2025-08-09 16:58:16 +0630

    initial commit

---
+++ app/js/app.js
@@ -0,0 +1,21 @@
+var user = { user: "Alice", age: 22 };
+
+function add(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    if (a > b) {
+        return a - b;
+    } else {
+        return b - a;
+    }
+}
+
+function show() {
+    //
+}
+
+function hide() {
+    //
+}
