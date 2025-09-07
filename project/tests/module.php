e712c3a482adc41682c10dfc562a480baaeac02b
Ei Maung <eimg@fairwayweb.com>
2025-09-07 16:45:54 +0630

    composer

---
+++ project/tests/module.php
@@ -0,0 +1,21 @@
+<?php
+
+include("../vendor/autoload.php");
+
+use Libs\Database\MySQL;
+use Libs\Database\UsersTable;
+use Helpers\Auth;
+use Helpers\HTTP;
+use Faker\Factory as Faker;
+
+$mysql = new MySQL;
+$mysql->connect();
+
+$table = new UsersTable;
+$table->insert();
+
+Auth::check();
+HTTP::redirect();
+
+$faker = Faker::create();
+echo $faker->name;
