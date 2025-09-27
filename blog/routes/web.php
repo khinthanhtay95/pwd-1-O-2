a713a4e2a12180b60e0c95576b640d41a508c83a
Ei Maung <eimg@fairwayweb.com>
2025-09-27 16:49:12 +0630

    laravel

---
+++ blog/routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\ArticleController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/articles', [ArticleController::class, 'index']);
+Route::get('/articles/detail/{id}', [ArticleController::class, 'detail']);
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
